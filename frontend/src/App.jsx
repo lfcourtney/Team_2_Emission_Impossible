7085ec2e0ce692cc4e74d3b72a2e5ea2a4bde19d
Alex Price <alex.price@version1.com>
2026-01-13 14:01:11 +0000

    Tested tailwind in app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,8 @@ function App() {
 
   return (
     <>
-      <div>
+      <div className="flex justify-center items-center mb-4">
+
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
